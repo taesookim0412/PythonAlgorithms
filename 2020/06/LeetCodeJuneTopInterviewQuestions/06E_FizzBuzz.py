4b0c9d9b23612722d7c2f651ac7888afbccca14d
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-06-02 10:37:36 -0700

    Much better

--- 2020/06/LeetCodeJuneTopInterviewQuestions/06E_FizzBuzz.py
+++ 2020/06/LeetCodeJuneTopInterviewQuestions/06E_FizzBuzz.py
@@ -1,22 +1,22 @@
 from typing import List
 
-#Runtime: 40 ms, faster than 80.27% of Python3 online submissions for Fizz Buzz.
-#Memory Usage: 14.8 MB, less than 6.38% of Python3 online submissions for Fizz Buzz.
+#Runtime: 36 ms, faster than 94.17% of Python3 online submissions for Fizz Buzz.
+#Memory Usage: 14.9 MB, less than 6.38% of Python3 online submissions for Fizz Buzz.
 #LOL
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         resp = ["Fizz", "Buzz", "FizzBuzz", ""]
-        return [self.switchData(i, resp) for i in range(1, n+1)]
+        return [resp[self.switchData(i, resp)] for i in range(1, n+1)]
 
     def switchData(self, i, resp) -> str:
         if i % 3 == 0 and i % 5 == 0:
-            return resp[2]
+            return 2
         if i % 3 == 0:
-            return resp[0]
+            return 0
         if i % 5 == 0:
-            return resp[1]
+            return 1
         resp[3] = str(i)
-        return resp[3]
+        return 3
 
 print(Solution().fizzBuzz(15))
