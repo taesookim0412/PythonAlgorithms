759891e3bed89fa4165abcb4b4212e4473923a69
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-06-14 11:19:59 -0700

    3unit math classes belong in 5 week sessions

---
+++ 2020/06/LeetCodeJuneTopInterviewQuestions/31E_NumberOf1Bits.py
@@ -0,0 +1,18 @@
+#Runtime: 32 ms, faster than 54.60% of Python3 online submissions for Number of 1 Bits.
+#Memory Usage: 13.8 MB, less than 71.68% of Python3 online submissions for Number of 1 Bits.
+
+
+#BitAnd then bitshift mask.
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        mask = 1
+        ones = 0
+        for i in range(32):
+            if n & mask != 0:
+                ones += 1
+            mask <<= 1
+        return ones
+#TODO: Study bit trick (Solution #2)
+
+s = Solution()
+print(s.hammingWeight(0o0000000000000000000000000001011))
