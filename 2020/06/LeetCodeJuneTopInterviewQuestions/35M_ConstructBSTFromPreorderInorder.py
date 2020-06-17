c03c503d39e8639363daf07452b15044d93dcf5f
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-06-17 09:15:21 -0700

    Construct a binary search tree from preorder and inorder traversal

--- 2020/06/LeetCodeJuneTopInterviewQuestions/35M_ConstructBSTFromPreorderInorder.py
+++ 2020/06/LeetCodeJuneTopInterviewQuestions/35M_ConstructBSTFromPreorderInorder.py
@@ -1,12 +1,38 @@
-#HOLDUP this is the wrong thing
+from typing import List
+
+#TODO: Iterator and Map
+#Basically this algorithms works by splitting the inorder list by the root.
+#Recursively calls itself in order to assign root treenodes on the first preorder element.
+#IT basically follows the layout of a preorder traversal but it indexes inorder elements based preorder[0]
+#however slicing inorder allows for a correctly balanced tree.
+#Example of when you just try to pop preorder: root.right = None
+#Can't slice preorder.
+
+#Runtime: 152 ms, faster than 60.21% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+#Memory Usage: 52.1 MB, less than 53.84% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        self.order = preorder[::-1]
-        self.root = TreeNode(self.order.pop())
-        root = self.root
-        for i in range(len(preorder)):
-            if not root:
-                root = TreeNode(self.order.pop())
+        if inorder:
+            ind = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder)
+            root.right = self.buildTree(preorder, inorder)
+            return root
 
 
+#Sample 60ms solution:
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        idmax = {val: idx for idx, val in enumerate(inorder)}
+
+        def helper(start, end):
+            if start > end:
+                return None
+
+            root = TreeNode(preorder.pop(0))
+            root.left = helper(start, idmax[root.val] - 1)
+            root.right = helper(idmax[root.val] + 1, end)
+            return root
+
+        return helper(0, len(preorder) - 1)
