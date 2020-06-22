5590c071299c61d5e29bc029728f2cb0bf4a7d84
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-06-22 13:44:30 -0700

    All I see is signs

--- 2020/06/LeetCodeJuneTopInterviewQuestions/39M_PalindromePartitioning.py
+++ 2020/06/LeetCodeJuneTopInterviewQuestions/39M_PalindromePartitioning.py
@@ -1,10 +1,56 @@
 from typing import List
+#"aa", "b"
+#"a", "ab"
 
+
+#Runtime: 80 ms, faster than 75.51% of Python3 online submissions for Palindrome Partitioning.
+#Memory Usage: 14.1 MB, less than 46.63% of Python3 online submissions for Palindrome Partitioning.
+
+#TODO: Make an original solution. Recursion in order to receive substring permutations. Very interesting.
+#Seems the pop solution is the standard solution here...
+#
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        def dfs(s, path, res):
+            if not s:
+                res.append(path[:])
+                return
+            for i in range(1, len(s)+1):
+                if s[:i] == s[i-1::-1]:
+                    path.append(s[:i])
+                    dfs(s[i:], path, res)
+                    path.pop()
         res = []
-        self.dfs(s, [], res)
+        dfs(s, [], res)
+        return res
 
-    def dfs(self, data, param):
-        pass
+class Solution1:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        for i in range(1, len(s)+1):
+            #[['a', 'a', 'b'], ['a', 'ab'], ['aa', 'b'], ['aab']]
+            if s[:i] == s[i-1::-1]:
+                for data in self.partition(s[i:]):
+                    res.append([s[:i]]+data)
+                    print(s[:i], data)
+        if not res:
+            return [[]]
+        return res
+
+
+class Solution2:
+    def partition(self, s: str) -> List[List[str]]:
+        data = []
+        for i in range(1, len(s)+1):
+            if s[:i] == s[i-1::-1]:
+                for x in self.partition(s[i:]):
+                    print(x)
+                    data.append([s[:i]] + x)
+        if not data:
+            return [[]]
+        return data
+
+
+s = Solution()
+print(s.partition("aab"))
