bbbc0962d8890d1978036bafcaefa9a6dc4a330b
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-06-25 11:59:42 -0700

    홀린 듯 날 따라와

--- 2020/06/LeetCodeJuneTopInterviewQuestions/40E_SymmetricTree.py
+++ 2020/06/LeetCodeJuneTopInterviewQuestions/40E_SymmetricTree.py
@@ -4,6 +4,19 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+#Runtime: 64 ms, faster than 7.16% of Python3 online submissions for Symmetric Tree.
+#Memory Usage: 14.1 MB, less than 15.32% of Python3 online submissions for Symmetric Tree.
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(root1, root2):
+            if root1 is None and root2 is None: return True
+            if root1 is None or root2 is None: return False
+            return root1.val == root2.val and isMirror(root1.left, root2.right) and isMirror(root1.right, root2.left)
+        if not root: return True
+        return isMirror(root.left, root.right)
+
+
 
 
 #okay well apparantely they wanted to know if it was symmetric about the root
