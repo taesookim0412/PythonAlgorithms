708443368a1e036d06aa6332818eae086f16ebea
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-06-27 13:32:08 -0700

    더 아름다운 햇살 쏟아질 때 더 가까워질 빛이 더 뜨거워질 꿈이 함께해 Together

--- 2020/06/LeetCodeJuneChallenge/27M_PerfectSquares.py
+++ 2020/06/LeetCodeJuneChallenge/27M_PerfectSquares.py
@@ -8,17 +8,33 @@ from math import sqrt
 
 #pretty interesting problem.
 #Many different possible approaches.
-#DFS, DP, Langrange's Four Square Theorem, None of which you'll probably understand when you first see it!
+#DFS, DP, Lagrange's Four Square Theorem, None of which you'll probably understand when you first see it!
 #TODO: Review Sum Of Perfect Squares
 class Solution:
     def numSquares(self, n: int) -> int:
         dp = [x for x in range(n+1)]
         for i in range(1, n+1):
             for j in range(1, int(sqrt(i))+1):
-                print(i-j*j)
+                print(i, j,i-j*j, dp)
+
                 dp[i] = min(dp[i], dp[i - j*j] + 1)
         print(dp)
         return dp[n]
+s = Solution()
+#print(s.numSquares(16))
+
+
+#Recursive
+class SolutionR:
+    def numSquares(self, n: int) -> int:
+        if n < 4: return n
+        ct = 9999999
+        for i in range(1, int(sqrt(n)+1)):
+            print(n-i*i)
+            ct = min(ct, self.numSquares(n-i*i) + 1)
+        return ct
+sr = SolutionR()
+print(sr.numSquares(5))
 
 #Tried
 # class Solution:
@@ -37,6 +53,3 @@ class Solution:
 #                 a = squares[i]
 #                 if a + squares[-1-i] == n or a == n:
 #                     return i+1
-s = Solution()
-print(s.numSquares(4))
-
