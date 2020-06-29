348f2b8a38e50424691f62e26270134477e943fc
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-06-28 17:43:54 -0700

    getting there

--- 2020/06/LeetCodeJuneChallenge/28M_ReconstructItinerary.py
+++ 2020/06/LeetCodeJuneChallenge/28M_ReconstructItinerary.py
@@ -1,23 +1,32 @@
 import collections
 import heapq
 from typing import List
-#66.5
-#51.42
+
+
+
+#Runtime: 80 ms, faster than 81.65% of Python3 online submissions for Reconstruct Itinerary.
+#Memory Usage: 14 MB, less than 84.32% of Python3 online submissions for Reconstruct Itinerary.
+
+#heap solution
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        targets = collections.defaultdict(list)
-        for a, b in sorted(tickets)[::-1]:
-            targets[a] += b,
-        route, stack = [], ['JFK']
-        while stack:
-            while targets[stack[-1]]:
-                stack += targets[stack[-1]].pop(),
-            route += stack.pop(),
-        return route[::-1]
-
-class Solution1:
-    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        targets = collections.defaultdict(list)
+        ticketsMap = collections.defaultdict(dict)
+        for i, a in enumerate(tickets):
+            if a[0] not in ticketsMap:
+                ticketsMap[a[0]] = [a[1]]
+            else:
+                heapq.heappush(ticketsMap[a[0]], a[1])
+        print(ticketsMap)
+        dest = ["JFK"]
+        visited = []
+        while dest:
+            while ticketsMap[dest[-1]]:
+                dest += heapq.heappop(ticketsMap[dest[-1]]),
+                print(f"dest: {dest}")
+            #not in map
+            visited += dest.pop(-1),
+            print(f"visited: {visited}")
+        return visited[::-1]
 
 
 #17/80 doesn't visit every airport
@@ -42,7 +51,20 @@ class Solution2:
 
         return visited
 
-
+#pochmann sol
+#66.5
+#51.42
+# class Solution:
+#     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+#         targets = collections.defaultdict(list)
+#         for a, b in sorted(tickets)[::-1]:
+#             targets[a] += b,
+#         route, stack = [], ['JFK']
+#         while stack:
+#             while targets[stack[-1]]:
+#                 stack += targets[stack[-1]].pop(),
+#             route += stack.pop(),
+#         return route[::-1]
 
 s = Solution()
 print(s.findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
