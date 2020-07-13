edd6da58527adc614a7b831ec6f39c1f9efb80ba
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-07-12 19:16:40 -0700

    Google intern online assessment questions

--- 2020/07/GoogleAssessmentQuestions/02E_CompareStrings.py
+++ 2020/07/GoogleAssessmentQuestions/02E_CompareStrings.py
@@ -7,38 +7,53 @@ from typing import List
 #[3,2]
 #3 strings with frequency smaller than B_0,
 #2 strings with frequency smaller than B_1
-
+#Smaller when the frequency of occurnece is less than freq in larger string
 
 class Solution:
-    def compareStrings(self, a:List[str], b:List[str]):
-        res = [0 for _ in range(len(b))]
-        a.sort()
-        for i in range(len(b)):
-            a = b[i]
+    def compareStrings(self, aL:List[str], bL:List[str]):
+        res = [0 for _ in range(len(bL))]
+        aL.sort()
+        for i in range(len(bL)):
+            a = bL[i]
             l = 0
             r = len(a)-1
             while l < r:
                 mid = l + (r-l)//2
-                if a[mid] <= a:
-                    l = mid + 1
-                else:
-                    r = mid
-            res[i] = l + 1
-
+                #I dont think this will work anymore, it could use a two pointer approach though.
+            res[i] = l
         return res
 
 
 #O(N^2) Runtime
 class Solution2:
-    def compareStrings(self, a:List[str], b:List[str]):
-        res = [0 for _ in range(len(b))]
-        for i in range(len(b)):
-            a = b[i]
-            for j in a:
-                if j < a:
-                    res[i] += 1
+    def compareStrings(self, aL:List[str], bL:List[str]):
+        res = [0 for _ in range(len(bL))]
+        for i in range(len(bL)):
+            b = bL[i]
+            for j in aL:
+                res[i] += self.countFrequencies(b, j)
         return res
+    #a = b[i], j = a
+    def countFrequencies(self, b, j):
+        if b[0] < j[0]:
+            return 1
+        elif b[0] > j[0]:
+            return 0
+        char = b[0]
+        bCt = b.count(char)
+        jCt = j.count(char)
+        print(bCt, jCt)
+        if bCt > jCt:
+            return 1
+        elif bCt < jCt:
+            return 0
+        if b > j:
+            return 1
+        return 0
 
 
-s = Solution()
+
+
+
+s = Solution2()
 print(s.compareStrings(["abcd","aabc","bd"],["aaa","aa"]))
