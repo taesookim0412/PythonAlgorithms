e54a24af8a7447b93dfd75187449c386bad50afa
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-07-27 03:08:42 -0700

    ‼

--- 2020_/07/LeetCodeBinarySearch/20_FindPeakElement.py
+++ 2020_/07/LeetCodeBinarySearch/20_FindPeakElement.py
@@ -2,7 +2,21 @@ import collections
 import numpy as np
 from typing import List
 
+#86%
 class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums) == 1: return 0
+        if len(nums) == 2: return nums.index(max(nums))
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + (r -l ) // 2
+            if nums[mid] < nums[mid+1]:
+                l = mid + 1
+            else:
+                r = mid
+        return l
+
+class Solution2:
     def findPeakElement(self, nums: List[int]) -> int:
         if len(nums) == 1: return 0
         l, r = 0, len(nums) - 1
