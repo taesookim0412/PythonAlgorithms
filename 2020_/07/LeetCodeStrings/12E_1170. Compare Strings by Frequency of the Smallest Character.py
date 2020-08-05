cd0572b1e72a01223bb5c56ecf67a2a514179942
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-08-05 01:27:12 -0700

    ↔ Lockdown Month 5

--- 2020_/07/LeetCodeStrings/12E_1170. Compare Strings by Frequency of the Smallest Character.py
+++ 2020_/07/LeetCodeStrings/12E_1170. Compare Strings by Frequency of the Smallest Character.py
@@ -1,13 +1,43 @@
 import collections
-from bisect import bisect
+import bisect
 
 import numpy as np
 from typing import List
+
+#Runtime: 72 ms, faster than 91.49% of Python3 online submissions for Compare Strings by Frequency of the Smallest Character.
+#Memory Usage: 14.4 MB, less than 14.81% of Python3 online submissions for Compare Strings by Frequency of the Smallest Character.
+
+#This is binary searching a sorted list of counts.
+class Solution:
+    def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
+        res = []
+        wordCts = sorted(x.count(min(x)) for x in words)
+        for q in queries:
+            idx = q.count(min(q))
+            s = bisect.bisect(wordCts, idx)
+            res += len(wordCts) - s,
+        return res
+
+
+
 #Runtime: 60 ms, faster than 99.59% of Python3 online submissions for Compare Strings by Frequency of the Smallest Character.
 #Memory Usage: 14.2 MB, less than 66.67% of Python3 online submissions for Compare Strings by Frequency of the Smallest Character.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ?
-class Solution:
+class Solution2:
     def numSmallerByFrequency(self, queries, words):
         W = sorted([w.count(min(w)) for w in words])
         res = []
@@ -20,7 +50,7 @@ class Solution:
 #Brute force
 #Runtime: 560 ms, faster than 50.00% of Python3 online submissions for Compare Strings by Frequency of the Smallest Character.
 #Memory Usage: 14.3 MB, less than 62.96% of Python3 online submissions for Compare Strings by Frequency of the Smallest Character.
-class Solution:
+class Solution3:
     def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
         res = []
         wordFreqs = [self.getFrequency(word) for word in words]
