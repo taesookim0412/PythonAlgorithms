44ef62c670a9c88f1983ee1ee84d45fd3a58e99e
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-08-15 12:43:50 -0700

    ''

---
+++ 2020_/08/19E_819. Most Common Word.py
@@ -0,0 +1,32 @@
+import collections
+import numpy as np
+from typing import List
+
+#"j. t? T. z! R, v, F' x! L; l! W. M; S. y? r! n; O. q; I? h; w. t; y; X? y, p. k! k, h, J, r? w! U! V; j' u; R! z. s. T' k. P? M' I' j! y. P, T! e; X. w? M! Y, X; G; d, X? S' F, K? V, r' v, v, D, w, K! S? Q! N. n. V. v. t? t' x! u. j; m; n! F, V' Y! h; c! V, v, X' X' t? n; N' r; x. W' P? W; p' q, S' X, J; R. x; z; z! G, U; m. P; o. P! Y; I, I' l' J? h; Q; s? U, q, x. J, T! o. z, N, L; u, w! u, S. Y! V; S? y' E! O; p' X, w. p' M, h! R; t? K? Y' z? T? w; u. q' R, q, T. R? I. R! t, X, s? u; z. u, Y, n' U; m; p? g' P? y' v, o? K? R. Q? I! c, X, x. r' u! m' y. t. W; x! K? B. v; m, k; k' x; Z! U! p. U? Q, t, u' E' n? S' w. y; W, x? r. p! Y? q, Y. t, Z' V, S. q; W. Z, z? x! k, I. n; x? z; V? s! g, U; E' m! Z? y' x? V! t, F. Z? Y' S! z, Y' T? x? v? o! l; d; G' L. L, Z? q. w' r? U! E, H. C, Q! O? w! s? w' D. R, Y? u. w, N. Z? h. M? o, B, g, Z! t! l, W? z, o? z, q! O? u, N; o' o? V; S! z; q! q. o, t! q! w! Z? Z? w, F? O' N' U' p? r' J' L; S. M; g' V. i, P, v, v, f; W? L, y! i' z; L? w. v, s! P?"
+#["m","q","e","l","c","i","z","j","g","t","w","v","h","p","d","b","a","r","x","n"]
+
+
+#Runtime: 36 ms, faster than 73.21% of Python3 online submissions for Most Common Word.
+#Memory Usage: 13.9 MB, less than 30.71% of Python3 online submissions for Most Common Word.
+
+import re
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph = paragraph.lower()
+        symbols = {'!','?',',',';','.'}
+        #\ |\!|\?|\,|\;|\.
+        words = filter(None, map(lambda word: word[:-1] if len(word) > 0 and word[-1] in symbols else word, re.split('\W+', paragraph)))
+        bannedWords = set(banned)
+        wordCt = sorted(collections.Counter(words).items(), key=lambda pair: pair[1])[::-1]
+        for word in wordCt:
+            if word and word[0] not in bannedWords:
+                return word[0]
+
+s = Solution()
+print(s.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.",
+["hit"]))
+print(s.mostCommonWord("Bob!",
+["hit"]))
+
+print(s.mostCommonWord("j. t? T. z! R, v, F' x! L; l! W. M; S. y? r! n; O. q; I? h; w. t; y; X? y, p. k! k, h, J, r? w! U! V; j' u; R! z. s. T' k. P? M' I' j! y. P, T! e; X. w? M! Y, X; G; d, X? S' F, K? V, r' v, v, D, w, K! S? Q! N. n. V. v. t? t' x! u. j; m; n! F, V' Y! h; c! V, v, X' X' t? n; N' r; x. W' P? W; p' q, S' X, J; R. x; z; z! G, U; m. P; o. P! Y; I, I' l' J? h; Q; s? U, q, x. J, T! o. z, N, L; u, w! u, S. Y! V; S? y' E! O; p' X, w. p' M, h! R; t? K? Y' z? T? w; u. q' R, q, T. R? I. R! t, X, s? u; z. u, Y, n' U; m; p? g' P? y' v, o? K? R. Q? I! c, X, x. r' u! m' y. t. W; x! K? B. v; m, k; k' x; Z! U! p. U? Q, t, u' E' n? S' w. y; W, x? r. p! Y? q, Y. t, Z' V, S. q; W. Z, z? x! k, I. n; x? z; V? s! g, U; E' m! Z? y' x? V! t, F. Z? Y' S! z, Y' T? x? v? o! l; d; G' L. L, Z? q. w' r? U! E, H. C, Q! O? w! s? w' D. R, Y? u. w, N. Z? h. M? o, B, g, Z! t! l, W? z, o? z, q! O? u, N; o' o? V; S! z; q! q. o, t! q! w! Z? Z? w, F? O' N' U' p? r' J' L; S. M; g' V. i, P, v, v, f; W? L, y! i' z; L? w. v, s! P?",
+["m","q","e","l","c","i","z","j","g","t","w","v","h","p","d","b","a","r","x","n"]))
---
+++ 2020_/08/Playground/comma_in_index.py
@@ -0,0 +1,11 @@
+import collections
+import numpy as np
+from typing import List
+
+import numpy
+
+data = [[0,1,2],[0, 1, 2, 3, 4]]
+
+#This is a tuple
+#print(data[0, :1])
+
