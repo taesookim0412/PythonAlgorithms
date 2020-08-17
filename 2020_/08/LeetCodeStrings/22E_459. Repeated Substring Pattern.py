e688a0da600c5069fa0543e2dea82dc5c72edf23
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-08-16 20:44:22 -0700

    (

--- 2020_/08/LeetCodeStrings/22E_459. Repeated Substring Pattern.py
+++ 2020_/08/LeetCodeStrings/22E_459. Repeated Substring Pattern.py
@@ -1,11 +1,50 @@
 import collections
 import numpy as np
 from typing import List
+import math
+
+#Runtime: 440 ms, faster than 9.59% of Python3 online submissions for Repeated Substring Pattern.
+#Memory Usage: 14 MB, less than 30.69% of Python3 online submissions for Repeated Substring Pattern.
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        if len(s) == 1: return False
+        sList = list(s)
+        ls = list(s[:math.ceil(len(s)/2)])
+        for i in range(len(ls) - 1, -1, -1):
+            #print(ls)
+            #i+1 or len(ls)
+            if len(s)%len(ls) == 0:
+                times = len(s) // len(ls)
+                ls2 = []
+                ls2 += [item for _ in range(times) for item in ls]
+                if ls2 == sList:
+                    return True
+                #print(ls2, "\n", sList)
+            ls.pop()
+        return False
+
+#Our termination takes too long, reverse the loop
+#Runtime: 84 ms, faster than 55.68% of Python3 online submissions for Repeated Substring Pattern.
+#Memory Usage: 14.1 MB, less than 12.03% of Python3 online submissions for Repeated Substring Pattern.
+class Solution2:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        ls = ""
+        rs = list(s)[::-1]
+        for i in range(1, (len(s)//2)+1):
+            ls += rs.pop()
+            #if i rem len
+            if len(s)%i == 0:
+                times = len(s) // i
+                ls2 = ls * times
+                if ls2 == s:
+                    return True
+        return False
 
 #Runtime: 40 ms, faster than 82.89% of Python3 online submissions for Repeated Substring Pattern.
 #Memory Usage: 14.1 MB, less than 12.65% of Python3 online submissions for Repeated Substring Pattern.
 
-class Solution:
+class Solution99:
     def repeatedSubstringPattern(self, s: str) -> bool:
         for i in range(len(s)//2, 0, -1):
             if len(s)%i == 0:
@@ -17,7 +56,7 @@ class Solution:
 #Runtime: 872 ms, faster than 6.48% of Python3 online submissions for Repeated Substring Pattern.
 #Memory Usage: 13.9 MB, less than 40.33% of Python3 online submissions for Repeated Substring Pattern.
 
-class Solution2:
+class Solution4:
     def repeatedSubstringPattern(self, s: str) -> bool:
         ls = ""
         rs = list(s)[::-1]
@@ -35,3 +74,15 @@ class Solution2:
 s = Solution()
 print(s.repeatedSubstringPattern("abab"))
 print(s.repeatedSubstringPattern("ababab"))
+print(s.repeatedSubstringPattern("abcdabra"))
+#True
+print(s.repeatedSubstringPattern("babbabbabbabbab"))
+print(s.repeatedSubstringPattern("zzz"))
+print(s.repeatedSubstringPattern("a"))
+
+
+
+
+#testStr = "sssss"
+#print(len(testStr)//2)
+#print(testStr[:math.ceil(len(testStr)/2)])
