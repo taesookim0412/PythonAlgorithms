6b5693b8951a2127ac376c83fc3bff8e1e026411
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-09-14 11:54:16 -0700

    >

--- 2020_/09/LeetCodeStrings/09M_1324. Print Words Vertically.py
+++ 2020_/09/LeetCodeStrings/09M_1324. Print Words Vertically.py
@@ -17,22 +17,27 @@ from typing import List
 # inverted for loop
 
 
-# 2/32
 # Input
 # "TO BE OR NOT TO BE"
 # Output
 # ["TBONTB","OEROOE","T"]
 # Expected
 # ["TBONTB","OEROOE","   T"]
+
+# Runtime: 36 ms, faster than 33.18% of Python3 online submissions for Print Words Vertically.
+# Memory Usage: 13.8 MB, less than 61.22% of Python3 online submissions for Print Words Vertically.
 def printVertically(self, s: str) -> List[str]:
     words = list(filter(None, s.split(' ')))
     longest_Len = len(max(words, key=len))
-    res = ['' for _ in range(longest_Len)]
+    res = [[[' '] for _ in range(len(words))] for _ in range(longest_Len)]
     for i in range(longest_Len):
         for j in range(len(words)):
-            res[i] += words[j][i] if len(words[j]) > i else ''
-    return res
+            res[i][j] = words[j][i] if len(words[j]) > i else ' '
+    # just remove trailing spaces bro
+
+    return [''.join(x).rstrip() for x in res]
 
 
-printVertically(0, "HOW ARE YOU")
-printVertically(0, "HOW ARE YOUU")
+print(printVertically(0, "HOW ARE YOU"))
+print(printVertically(0,   "HOW ARE YOUU"))
+print(printVertically(0,   "TO BE OR NOT TO BE"))
