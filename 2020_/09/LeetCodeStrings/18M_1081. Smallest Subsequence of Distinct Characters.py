3612ca29848218ffadb09339217827721f186c62
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-09-24 17:55:25 -0700

    E

--- 2020_/09/LeetCodeStrings/18M_1081. Smallest Subsequence of Distinct Characters.py
+++ 2020_/09/LeetCodeStrings/18M_1081. Smallest Subsequence of Distinct Characters.py
@@ -3,8 +3,19 @@ import numpy as np
 from typing import List
 
 
-class Solution:
+# Runtime: 32 ms, faster than 74.37% of Python3 online submissions for Smallest Subsequence of Distinct Characters.
+# Memory Usage: 13.9 MB, less than 49.75% of Python3 online submissions for Smallest Subsequence of Distinct Characters.
 
+class Solution:
+    def smallestSubsequence(self, s) -> str:
+        last_occurences = {s: i for i, s in enumerate(s)}
+        stack = []
+        for i, c in enumerate(s):
+            if c in stack: continue
+            while stack and stack[-1] > c and i < last_occurences[stack[-1]]:
+                stack.pop()
+            stack.append(c)
+        return ''.join(stack)
 
     #smallest substring with varient length (redundant)(question also asked for subsequence)
     def smallestSubsequence(self, s) -> str:
