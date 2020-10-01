677cd752d646c7bd997a78aa320bbac1cbacb3a6
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-10-01 04:02:11 -0700

    J They call it the NVidia Tesla because they get a tesla

--- 2020_/09/LeetCodeBinaryTree/02M_144. Binary Tree Preorder Traversal.py
+++ 2020_/09/LeetCodeBinaryTree/02M_144. Binary Tree Preorder Traversal.py
@@ -2,21 +2,33 @@ import collections
 import numpy as np
 from typing import List
 #Iterative
+#78.11 %
+
+#Append the tree nodes -> Iterate a stack with dynamic R->L append
+#This contrasts the idea of looping and logging the current root node
+#instead the only way the stack would work is to place side nodes beforehand
+#else it would endlessly loop and log its lower left node
+#(if created similar to a traditional recursive loop).
 class Solution:
+    def iterativeTraversal(self, root: TreeNode) -> List[int]:
+        if not root: return []
+        stack = [root]
+        res = []
+        while stack:
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+        return res
     def preorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
-        # def preorderTraversal(self, root: TreeNode) -> List[int]:
-        #     stack = [root]
-        #     res = []
-        #     while stack:
-        #         curr = stack.pop()
-        #         if curr:
-        #             res.append(curr.val)
-        #             stack.append(curr.left)
         def recursivePreorder(tree):
             if tree:
                 res.append(tree.val)
                 recursivePreorder(tree.left)
                 recursivePreorder(tree.right)
+        #recursivePreorder(root)
         recursivePreorder(root)
         return res
